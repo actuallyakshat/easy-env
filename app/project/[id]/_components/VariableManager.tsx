0924587ed4156d91a11c3f19181b334496c819c1
Akshat Dubey <akshatdubey0808@gmail.com>
2024-12-09 16:30:44 +0530

    fix: removed unused import

--- app/project/[id]/_components/VariableManager.tsx
+++ app/project/[id]/_components/VariableManager.tsx
@@ -1,21 +1,5 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { saveAs } from "file-saver";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -27,7 +11,22 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { saveAs } from "file-saver";
 import { Download, Edit, Eye, EyeOff, Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 import { toast } from "sonner";
 import {
